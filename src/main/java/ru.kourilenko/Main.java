1ca1af58b9ab75d3e48737ad0f1ffd7950ea9083
nastya <kankankan92@mail.ru>
2016-08-04 15:18:21 +0300

    начало

---
+++ src/main/java/ru.kourilenko/Main.java
@@ -0,0 +1,19 @@
+package ru.kourilenko;
+
+public class Main {
+    public static void main(String[] args) {
+//        byte a [] = {67};
+//        String s = new String(a);
+//        System.out.println(s);
+//        a[0] = 74;
+//        System.out.println(s);
+//        System.out.println(a);
+//        String d = "Calendar";
+//        System.out.println(d.charAt(2));
+//        System.out.println(d.getBytes()[0]);
+//        System.out.println("аАа".equalsIgnoreCase("a"));
+        String s [] = {"a", "b"};
+        System.out.println(s);
+
+    }
+}
---
+++ src/main/java/ru.kourilenko/StringUtil.java
@@ -0,0 +1,7 @@
+package ru.kourilenko;
+
+public class StringUtil {
+    void bubbleSort(int[] arr) {
+        
+    }
+}
